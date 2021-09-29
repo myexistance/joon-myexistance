70383cf9fcd08912a0459f60b1fb9b96eca95634
Joon Yoon <myexistance@HOTMAIL.COM>
2021-09-29 19:43:12 +1300

    template

---
+++ src/yourdata.js
@@ -0,0 +1,123 @@
+
+
+// Social Icon
+import githubIcon from "./images/github.svg"
+import instagramIcon from "./images/instagram.svg"
+
+export default {
+  //(Please Do Not Remove The comma(,) after every variable)
+  //Change The Website Template
+
+  //   Header Details ---------------------
+  name: "Joon Yoon",
+  headerTagline: [
+    //Line 1 For Header
+    "Building digital",
+    //Line 2 For Header
+    "products, brands,",
+    //Line 3 For Header
+    "and experience",
+  ],
+  //   Header Paragraph
+  headerParagraph:
+    "Hi, I am a Full Stack Developer from Wellington, New Zealand. I’m interested in frontend and full-stack development roles. I enjoy building everything from small business sites to IoT interactive apps. Let's collaborate",
+
+  //Contact Email
+  contactEmail: "joonyoonlives@gmail.com",
+
+  // End Header Details -----------------------
+
+  // Work Section ------------------------
+  projects: [
+    {
+      title: "Rank", //Project Title - Add Your Project Title Here
+      para:
+        "Public toilet finder to help those who are in the urgent.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://myexistance.github.io/joon-myexistance/static/media/RANK.0e28912a.png",
+      //Project URL - Add Your Project Url Here
+      url: "https://rank2021.herokuapp.com/",
+    },
+    {
+      title: "Project Two", //Project Title - Add Your Project Title Here
+      para:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1605153864431-a2795a1b2f95?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwyNTY3ODl8fGVufDB8fHw%3D&auto=format&fit=crop&w=400&q=60",
+      //Project URL - Add Your Project Url Here
+      url: "",
+    },
+    {
+      title: "Project Three", //Project Title - Add Your Project Title Here
+      para:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1504083898675-c896ecdae86e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjJ8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+      //Project URL - Add Your Project Url Here
+      url: "",
+    },
+    {
+      title: "Project Four", //Project Title - Add Your Project Title Here
+      para:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1492171983775-a51717616c0d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NjB8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+      //Project URL - Add Your Project Url Here
+      url: "",
+    },
+
+    /*
+    If You Want To Add More Project just Copy and Paste This At The End
+    ,{
+        title: 'Project Five',
+        para: 'Something Amazing',
+        imageSrc: "",
+        url: ''
+    }
+    */
+  ],
+
+  // End Work Section -----------------------
+
+  // About Secton --------------
+  aboutParaOne:
+    "Born in Korean and bred in New Zealand. I am interested in JAM-stack. I enjoy building apps simple and intuitive but also necessary. I relish any opportunity to work with new technologies while obtaining mastery with my other skills. Ultimately, I want to work on bringing inspirational ideas to life. I enjoy building everything from small business sites to IoT interactive apps.",
+  
+  aboutImage:
+    "https://images.unsplash.com/photo-1519713958759-6254243c4a53?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+
+  //   End About Section ---------------------
+
+
+  //   Promotion Section --------------------------
+
+  
+  promotionPara:
+    "I’m never satisfied to just come up with ideas. Instead I have an almost impulsive need to act on them.",
+  // End Promotion Section -----------------
+
+
+  //   Contact Section --------------
+
+  contactSubHeading: "Let's create your next experience together",
+  social: [
+    // Add Or Remove The Link Accordingly
+    { img: githubIcon, url: "https://github.com/myexistance" },
+    //  {
+    //   img: linkedinIcon,
+    //   url: "https://www.linkedin.com/in/myexistance",
+    // },
+    {
+      img: instagramIcon,
+      url: "https://www.instagram.com/myexistance",
+    }
+  ],
+
+  // End Contact Section ---------------
+}
+
+// Thanks for using this template, I would love to hear from you contact me at hello@chetanverma.com
